969f4d99e5ec9df187d18ffb8b9998a3e46edf96
Saranya G <saranyagovindhasamy2607@gmail.com>
2025-06-05 11:17:42 +0530

    company info website day 1

---
+++ Day6/pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      
+      <h1>
+        <span className="welcome-animation">Welcome to Sakara</span>
+      </h1>
+      <p></p>
+      <p>
+         Sakara is dedicated to providing exceptional services with creativity and care.<br/>
+        Explore our site to learn more about what makes Sakara special!
+      </p>
+      
+    </div>
+  )
+}
