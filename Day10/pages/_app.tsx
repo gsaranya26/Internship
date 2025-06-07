643ccdc5b4e5a7b96765214bd7b5040eff09c726
Saranya G <saranyagovindhasamy2607@gmail.com>
2025-06-07 12:12:13 +0530

    complete website

--- Day10/pages/_app.tsx
+++ Day10/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
